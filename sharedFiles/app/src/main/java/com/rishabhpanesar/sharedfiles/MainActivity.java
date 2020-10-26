7e96f800f737dcc19771fe7261703e2937e67ff3
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-10-26 15:11:30 +0530

    App for learning file sharing

---
+++ sharedFiles/app/src/main/java/com/rishabhpanesar/sharedfiles/MainActivity.java
@@ -0,0 +1,17 @@
+package com.rishabhpanesar.sharedfiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
