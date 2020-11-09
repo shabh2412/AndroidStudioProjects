de7d51924857248171e8e0f50fde30a0c2ceda9a
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-11-09 09:43:43 +0530

    This is an app which retrieves Device Information

---
+++ MyPhone/app/src/main/java/com/rishabhpanesar/myphone/MainActivity.java
@@ -0,0 +1,58 @@
+package com.rishabhpanesar.myphone;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.rishabhpanesar.myphone.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private final static String TELEPHONY_SERVICE = Context.TELEPHONY_SERVICE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, PackageManager.PERMISSION_GRANTED);
+
+
+    }
+
+    public void getDetails(View view) {
+        TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(this.TELEPHONY_SERVICE);
+        String networkCountryISO = telephonyManager.getNetworkCountryIso();
+        String SIMCountryCode = telephonyManager.getSimCountryIso();
+        int phoneType = telephonyManager.getPhoneType();
+        switch (phoneType) {
+            case (TelephonyManager.PHONE_TYPE_CDMA):
+                binding.phoneType.setText("CDMA");
+                break;
+            case (TelephonyManager.PHONE_TYPE_GSM):
+                binding.phoneType.setText("GSM");
+                break;
+            case (TelephonyManager.PHONE_TYPE_SIP):
+                binding.phoneType.setText("SIP");
+                break;
+            case (TelephonyManager.PHONE_TYPE_NONE):
+                binding.phoneType.setText("None");
+                break;
+
+        }
+        binding.roaming.setText(telephonyManager.isNetworkRoaming() ? "Roaming" : "Not Roaming");
+
+        binding.networkCountryIso.setText(networkCountryISO);
+        binding.simCountryCode.setText(SIMCountryCode);
+//        binding.textView1.setText(info);
+//        binding.textView1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+    }
+}
