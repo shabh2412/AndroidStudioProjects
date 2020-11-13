828822e676510d29da5850c6f167e71a82dbbd99
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-11-14 03:23:46 +0530

    Added Resources, & used Material Theme, & changed a few text

--- MyDialog/app/src/main/java/com/rishabhpanesar/mydialog/MainActivity.java
+++ MyDialog/app/src/main/java/com/rishabhpanesar/mydialog/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,19 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void open(View view) {
+    public void open(final View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Is it interesting?");
-        builder.setPositiveButton("Yeah!", new DialogInterface.OnClickListener() {
+        builder.setMessage("This will end the activity");
+        builder.setPositiveButton("I agree", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity.this, "Interesting", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Interesting", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "Activity ended", Snackbar.LENGTH_SHORT).show();
             }
         });
-        builder.setNegativeButton("Nah", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("No no", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity.this, "NAH", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "Operation Cancelled", Snackbar.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "NAH", Toast.LENGTH_SHORT).show();
             }
         });
         AlertDialog alertDialog = builder.create();
